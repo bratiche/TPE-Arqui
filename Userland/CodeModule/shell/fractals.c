a125186a2cd45470d8ddd68062ac492b77d21e46
adebrouvier <ariel.debrouvier@gmail.com>
2016-06-21 19:45:51 -0300

    fractal source

--- Userland/CodeModule/shell/fractals.c
+++ Userland/CodeModule/shell/fractals.c
@@ -4,6 +4,7 @@
 //Fuente: http://www.splinter.com.au/converting-hsv-to-rgb-colour-using-c
 static void HsvToRgb(unsigned char *r, unsigned char *g, unsigned char *b, unsigned char h, unsigned char s, unsigned char v);
 
+//Fuente: http://warp.povusers.org/Mandelbrot/
 void mandelbrot(int iter, uint8_t r, uint8_t g, uint8_t b, uint8_t r2, uint8_t g2, uint8_t b2) {
 
     double MinRe = -2.0;
@@ -48,6 +49,7 @@ void mandelbrot(int iter, uint8_t r, uint8_t g, uint8_t b, uint8_t r2, uint8_t g
 
 }
 
+//Fuente: http://lodev.org/cgtutor/juliamandelbrot.html
 void juliaSet() {
 
     //each iteration, it calculates: new = old*old + c, where c is a constant and old starts at current pixel
